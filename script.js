b9384e06b395de58963f06221584fd4b6b70ccba
nienkevonk <84348574+nienkevonk@users.noreply.github.com>
2021-06-14 23:09:31 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+const wallpainting = function (paint, wall) {
+    console.log("The " + wall + " has been painted " + paint)
+};
+
+wallpainting("red", "south-east wall");
+wallpainting("green", "north wall");
+wallpainting("blue", "south wall");
